ef94aef4753999ce5e82ca283b7b18a965edebaa
Nicojasy <dimataranapa12@gmail.com>
2020-05-21 21:57:06 +0300

    save new config

---
+++ MeetingService/Config/AuthOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MeetingsService
+{
+    public class AuthOptions
+    {
+        public const string ADRESS = "email@gmail.com";
+        public const string ISSUER = "MeetingsService";
+        public const string AUDIENCE = "Attendee";
+        const string KEY = "super_key";
+        public const int LIFETIME = 1;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
